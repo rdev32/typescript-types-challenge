f638417efcf2b58a430e9400606b13dfbba27097
rdev32 <rdevelops32@gmail.com>
2023-04-30 16:35:17 -0500

    feat: add includes implementation

---
+++ questions/easy/includes/test-cases.ts
@@ -0,0 +1,11 @@
+import { Includes } from './template'
+
+type cases = [
+  Expect<
+    Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>
+  >,
+  Expect<
+    Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>
+  >,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
+]
